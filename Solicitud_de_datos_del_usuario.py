117943803633e0315c3c4819e15bd328d5267f33
LortizM97 <lu.ortizm@duocuc.cl>
2026-04-10 00:27:40 +0000

    evaluacion_n1_dry7122

---
+++ Solicitud_de_datos_del_usuario.py
@@ -0,0 +1,12 @@
+print("Ingreso de datos del estudiante")
+print("--------------------------------")
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo_seccion = input("Ingrese su Código-Sección: ")
+sede = input("Ingrese su sede: ")
+
+print("\n--- Información ingresada ---")
+print("Nombre completo:", nombre, apellido)
+print("Código-Sección:", codigo_seccion)
+print("Sede:", sede)
---
+++ integrantes.py
@@ -0,0 +1,8 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("-------------------------------------------------")
+
+print("Integrantes:")
+print("- Branco Solis")
+print("- Cristian Castro")
+print("- Luis Ortiz")
+print("- Francisco Soto-Aguilar")
